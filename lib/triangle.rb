ee86a5a0fb4c900b76e2efc88d20f3a1d068a941
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-22 14:31:05 -0600

    commit tree c1c9a48c74bf44ec91d34ed7eb3e14bc674dbde7

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c, :sides
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    @sides = [a, b, c]
+  end
+
+  def invalid?
+    sides.any? {|side| side <= 0} || (a + b <= c || a + c <=b || c + b <=a)
+  end
+
+
+  def kind
+    if invalid?
+      raise TriangleError
+    elsif sides.uniq.count == 1
+      :equilateral
+    elsif sides.uniq.count == 2
+      :isosceles
+    else
+      :scalene
+    end
+
+  end
+
+  class TriangleError < StandardError
+  end
 end
